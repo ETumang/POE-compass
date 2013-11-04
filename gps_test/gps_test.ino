bb225b9d1ee8d78726c03da55b6eb1028b3e8d8e
Thomas Chen <thomas.chen@students.olin.edu>
2013-11-03 20:35:49 -0500

    second commit moving folders

---
+++ gps_test/gps_test.ino
@@ -0,0 +1,132 @@
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303.h>
+
+
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// If using software serial (sketch example default):
+//   Connect the GPS TX (transmit) pin to Digital 3
+//   Connect the GPS RX (receive) pin to Digital 2
+// If using hardware serial (e.g. Arduino Mega):
+//   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
+//   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
+
+// If using software serial, keep these lines enabled
+// (you can change the pin numbers to match your wiring):
+#if ARDUINO >= 100
+  SoftwareSerial mySerial(3, 2);
+#else
+  NewSoftSerial mySerial(3, 2);
+#endif
+Adafruit_GPS GPS(&mySerial);
+// If using hardware serial (e.g. Arduino Mega), comment
+// out the above six lines and enable this line instead:
+//Adafruit_GPS GPS(&Serial1);
+
+#define GPSECHO  true
+
+boolean usingInterrupt = false;
+void useInterrupt(boolean);
+
+Adafruit_LSM303_Mag mag = Adafruit_LSM303_Mag(12345);
+Adafruit_LSM303_Accel accel = Adafruit_LSM303_Accel(54321);
+
+void setup()  
+{
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS/magnetometer library basic test!");
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  GPS.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data for high update rates!
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // uncomment this line to turn on all the available data - for 9600 baud you'll want 1 Hz rate
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
+  
+  // Set the update rate
+  // 1 Hz update rate
+  // GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  // 5 Hz update rate- for 9600 baud you'll have to set the output to RMC or RMCGGA only (see above)
+  // GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  // 10 Hz update rate - for 9600 baud you'll have to set the output to RMC only (see above)
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+  
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_FIFTHHZ);
+  // Remember to alter the library!
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+  
+  delay(3000);
+}
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+
+void loop()
+{
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  accel.getEvent(&event);
+  
+  float Pi = 3.14159;
+  
+  // Calculate the angle of the vector y,x
+  float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+  
+  // Normalize to 0-360
+  if (heading < 0)
+  {
+    heading = 360 + heading;
+  }
+  Serial.print("Compass Heading: ");
+  Serial.println(heading);
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  delay(100);
+}
---
+++ pseudo/pseudo.ino
@@ -0,0 +1,77 @@
+#include <Libraries.h>
+
+Mag mag = Mag(uniqueID); // initialize magnetometer
+
+int in1Pin = 12;
+int in2Pin = 11;
+int in3Pin = 10;
+int in4Pin = 9;
+Stepper stepper(720, in1Pin, in2Pin, in3Pin, in4Pin);  
+
+const float Pi = 3.1416;
+float targetHeading = 0.0;
+const int RFreq = 800;
+int updateInterval = 1000;
+
+void setup() {
+  RF.initialize();
+  GPS.initialize();
+  
+  Serial.begin(9600);
+  analogReference (EXTERNAL);
+  int timeOffset = RF.receive(initTime);
+  
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+  
+  stepper.setSpeed(10);
+}
+
+void loop() {
+  magX = mag.getX();
+  magY = mag.getY();
+  float currentHeading = (atan2(magY,magX) * 180) / Pi;
+  if (currentHeading < 0) {
+    currentHeading = currentHeading + 360;
+  }
+  
+  float latitude = GPS.getLat(); // Y
+  float longitude = GPS.getLong(); // X
+  if (longEAST) {
+    longitude = 360 - longitude;
+  }
+  float beaconGPS[5] = RF.receive(RFreq);
+  float beaconLat = beaconGPS[1];
+  float beaconLong = beaconGPS[2];
+  if (beaconEAST) {
+    beaconLong = 360 - beaconLong;
+  }
+  float deltaY = beaconLat - latitude;
+  float deltaX = beaconLong - longitude;
+  
+  targetHeading = (atan2(deltaY,deltaX) * 180) / Pi;
+  if (targetHeading < 0) {
+    targetHeading = targetHeading + 360;
+  }
+  float error = currentHeading - targetHeading;
+  if(error > 180) {
+    error = error - 360;
+  }
+  
+  stepper.step(error * 720 / 360 / 2);
+  delay(updateInterval);
+}
+
+// set up magnetometer
+// set up time offset
+// set up motor
+// initialize other instruments
+// loop magnetometer readings
+// loop convert magnetometer to heading
+// loop receive beacon signal (GPS coordinates)
+// loop convert self + beacon GPS into vector heading
+// loop compare current heading to target heading
+// loop correct motor for heading
+// Try to get rid of floating points later
